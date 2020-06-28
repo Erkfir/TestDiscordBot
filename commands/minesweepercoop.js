2398d81cec1a814916327ed7b8942ac5fc2ddbe5
Lydia Looi <llo35@uclive.ac.nz>
2020-06-29 00:39:49 +1200

    Added to the minesweepercoop command the ability to play a game in the channel. - need to make it so messages are edited rather than a new one sent - need to check if a bomb has been activated - need to add the option to flag - command messages deleted so to stop the spam

--- commands/minesweepercoop.js
+++ commands/minesweepercoop.js
@@ -1,6 +1,7 @@
 let emotes = require("../resources/emojiCharacters");
 emotes[9] = ":bomb:";
 emotes["blank"] = ":blue_square:";
+emotes["flag"] = ":triangular_flag_on_post:";
 
 const letters = {
     1: "a",
@@ -15,71 +16,135 @@ const letters = {
     10: "j"
 };
 
+const numbers = {
+    "a": 0,
+    "b": 1,
+    "c": 2,
+    "d": 3,
+    "e": 4,
+    "f": 5,
+    "g": 6,
+    "h": 7,
+    "i": 8,
+    "j": 9
+};
+
+let games = {};
+const numBombs = 10;
+const numRows = 10;
+const numCols = 10;
+
+function displayBoard(message, display, numRows, numCols) {
+    let output = "";
+    let letter = 1;
+
+    for (let i = 0; i < numRows; i++) {
+        output += emotes[letters[letter]];
+        letter++;
+
+        if (letter > 10) letter = 1;
+
+        for (let j = 0; j < numCols; j++) {
+            output += emotes[display[i][j]];
+        }
+        output += "\n";
+    }
+    output += emotes["blank"] + ":regional_indicator_a::regional_indicator_b::regional_indicator_c::regional_indicator_d::regional_indicator_e::regional_indicator_f::regional_indicator_g::regional_indicator_h::regional_indicator_i::regional_indicator_j:";
+
+    if (output.length > 2000) {
+        message.channel.send("The given grid is too large :(");
+        return null;
+    }
+    return output;
+}
+
 module.exports = {
     name: "minesweepercoop",
-    aliases: ["coopms"],
+    aliases: ["coopms", "cms"],
     description: "It's coop minesweeper",
-    cooldown: 5,
+    cooldown: 1,
     execute(message, args) {
-        let numBombs = 10;
-        let numRows = 10;
-        let numCols = 10;
 
-        let board = [];
+        if (args.length === 0) {
 
-        for (let rowIndex = 0; rowIndex < numRows; rowIndex++) {
-            let row = [];
-            for (let colIndex = 0; colIndex < numCols; colIndex++) {
-                row.push(0);
+
+            let board = [];
+            let display = [];
+
+            for (let rowIndex = 0; rowIndex < numRows; rowIndex++) {
+                let row = [];
+                let drow = [];
+                for (let colIndex = 0; colIndex < numCols; colIndex++) {
+                    row.push(0);
+                    drow.push("blank");
+                }
+                board.push(row);
+                display.push(drow);
             }
-            board.push(row);
-        }
 
-        let count = 0;
+            let count = 0;
 
-        while (count < numBombs) {
-            let row = Math.floor(Math.random() * numRows);
-            let col = Math.floor(Math.random() * numCols);
+            while (count < numBombs) {
+                let row = Math.floor(Math.random() * numRows);
+                let col = Math.floor(Math.random() * numCols);
 
-            if (board[row][col] < 9) {
-                board[row][col] = 9;
+                if (board[row][col] < 9) {
+                    board[row][col] = 9;
 
-                for (let i = -1; i <= 1; i++) {
-                    for (let j = -1; j <= 1; j++) {
-                        if ((row + i) >= 0 && (row + i) < numRows && ((col + j) >= 0) && (col + j) < numCols && !(i === 0 && j === 0)) {
-                            if (board[row + i][col + j] !== 9) {
-                                board[row + i][col + j]++;
+                    for (let i = -1; i <= 1; i++) {
+                        for (let j = -1; j <= 1; j++) {
+                            if ((row + i) >= 0 && (row + i) < numRows && ((col + j) >= 0) && (col + j) < numCols && !(i === 0 && j === 0)) {
+                                if (board[row + i][col + j] !== 9) {
+                                    board[row + i][col + j]++;
+                                }
                             }
                         }
                     }
+
+                    count++;
                 }
+            }
 
-                count++;
+            let output = displayBoard(message, display, numRows, numCols);
+
+            if (output == null) return;
+
+
+            message.channel.send(output).then((sentMessage) => {
+                    games[sentMessage.channel.id] = {
+                        "id": sentMessage.id,
+                        "board": board,
+                        "display": display,
+                    };
+                }
+            );
+        } else { // Play!
+            let channelId = message.channel.id;
+
+            if (!(channelId in games)) {
+                message.channel.send("There is no current running game in this channel!");
+                return;
+            }
+
+            try {
+                let row = numbers[args[0].toLowerCase()];
+                let col = numbers[args[1].toLowerCase()];
+
+                games[channelId].display[row][col] = games[channelId].board[row][col];
+                let output = displayBoard(message, games[channelId].display, numRows, numCols);
+                message.channel.send(output).then((sentMessage) => {
+                        games[sentMessage.channel.id] = {
+                            "id": sentMessage.id,
+                            "board": games[channelId].board,
+                            "display": games[channelId].display,
+                        };
+                    }
+                );
+
+            } catch (e) {
+                console.log("Something went wrong " + e);
+                message.channel.send("Something went wrong");
             }
         }
-
-        let output = "";
-        let letter = 1;
-
-        for (let i = 0; i < numRows; i++) {
-            output += emotes[letters[letter]];
-            letter++;
-
-            if (letter > 10) letter = 1;
-
-            for (let j = 0; j < numCols; j++) {
-                output += emotes["blank"];
-            }
-            output += "\n";
-        }
-        output += emotes["blank"] + ":regional_indicator_a::regional_indicator_b::regional_indicator_c::regional_indicator_d::regional_indicator_e::regional_indicator_f::regional_indicator_g::regional_indicator_h::regional_indicator_i::regional_indicator_j:"
-
-        if (output.length > 2000) {
-            message.channel.send("The given grid is too large :(");
-            return
-        }
-
-        message.channel.send(output);
-
     },
 };
